6adb6bd410e0f162085c150f6110d6f5f204e9cf
Laily Ajellu <lailyajellu@Lailys-MacBook-Air.local>
2016-06-12 03:49:17 -0400

    adds hacknews, add errorMsg

--- app.py
+++ app.py
@@ -2,6 +2,7 @@ from flask import Flask, request, redirect
 import twilio.twiml
 import json
 
+#Wiki Imports
 from Wiki import Wiki_feature
 
 #Weather Imports
@@ -11,6 +12,10 @@ import forecastio
 #Finance Imports
 from yahoo_finance import Share
 from finance import Finance
+
+#Hacker News Imports
+from hackernews import HackerNews
+from hacknews import HackNews
 
 app = Flask(__name__)
 
@@ -44,22 +49,61 @@ def hello_monkey():
             resp.message("Your query was not an exisiting wiki page, please use 'WIKI-FIND:' to retrieve the correct title and type it EXACTLY AS SHOWN")
         else:
             resp.message(summary_res)
+
     elif txt_msg[0] == "WEATHER:":
         del txt_msg[0]
         search_query = " ".join(txt_msg)
-
+       
         weatherObj = Weather(31.967819, 115.87718)
-        resp.message(weatherObj.displayWeather(txt_msg[0]))
-        resp.message(weatherObj.convertToJson())
+        resp.message(weatherObj.displayWeather(txt_msg[0])) #timeframe
+        
+
+    elif txt_msg[0] == "WEATHER:APP:":
+        del txt_msg[0]
+        search_query = " ".join(txt_msg)
+        
+        weatherObj = Weather(31.967819, 115.87718)
+        weatherObj.displayWeather(txt_msg[0])
+        resp.message(str(weatherObj.convertToJson()))
 
     elif txt_msg[0] == "FINANCE:":
         del txt_msg[0]
         search_query = " ".join(txt_msg)
 
-        financeObj = Finance(txt_msg[0]) #companyCode
-        resp.message(financeObj.displayFinance(txt_msg[1], txt_msg[2])) #yearStart, yearEnd
-        resp.message(financeObj.convertToJson())
+        try:
+            financeObj = Finance(txt_msg[0]) #companyCode
+            resp.message(financeObj.displayFinance(txt_msg[1], txt_msg[2])) #yearStart, yearEnd
+        except:
+            resp.message("The company code you chose doesn't exist, try YHOO, GOOG, AAPL, TWTR, AMZN\nText us: FINANCE: companyCode startYear endYear")
+
+    elif txt_msg[0] == "FINANCE:APP:":
+        del txt_msg[0]
+        search_query = " ".join(txt_msg)
+
+        try:
+            financeObj = Finance(txt_msg[0]) #companyCode
+            financeObj.displayFinance(txt_msg[1], txt_msg[2]) #yearStart, yearEnd
+            resp.message(str(financeObj.convertToJson()))
+        except:
+            resp.message("The company code you chose doesn't exist, try YHOO, GOOG, AAPL, TWTR, or AMZN")
+
     
+    elif txt_msg[0] == "HACKNEWS:":
+        del txt_msg[0]
+        search_query = " ".join(txt_msg)
+
+        hackNewsObj = HackNews()
+        resp.message(hackNewsObj.displayHackNews(txt_msg[0]))
+
+        
+    elif txt_msg[0] == "HACKNEWS:APP:":
+        del txt_msg[0]
+        search_query = " ".join(txt_msg)
+
+        hackNewsObj = HackNews()
+        hackNewsObj.displayHackNews(txt_msg[0])
+        resp.message(hackNewsObj.convertToJson())
+
     return str(resp)
 
 if __name__ == "__main__":
